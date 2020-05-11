7f3a054d63c74d1aa9a912c2e79fe6127d5bf899
kazuyuki.miyajima <miya0302.1015@gmail.com>
2020-05-11 11:35:59 +0900

    Add getAllMessages

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,15 +1,19 @@
 package controllers;
 
 import java.io.IOException;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class IndexServlet
- */
+import java.util.List;
+
+import models.Message;
+import utils.DBUtil;
+
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -26,8 +30,16 @@ public class IndexServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+	    EntityManager em = DBUtil.createEntityManager();
+
+        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class)
+                                   .getResultList();
+        response.getWriter().append(Integer.valueOf(messages.size()).toString());
+
+        em.close();
+
+
 	}
 
 }
